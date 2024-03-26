e61cfd11c8918d33ca3799c6ca2023eeedde79b4
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-03-26 12:09:53 +0530

    created custom button variants for the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<div>
-			<p className="text-green-500 font-bold">Hello Lingo</p>
+		<div className="flex flex-col p-24 gap-4">
+			<Button>DEFAULT</Button>
+			<Button variant="primary">PRIMARY</Button>
+			<Button variant="primaryOutline">PRIMARY OUTLINE</Button>
+			<Button variant="secondary">SECONDARY</Button>
+			<Button variant="secondaryOutline">SECONDARY OUTLINE</Button>
+			<Button variant="danger">DANGER</Button>
+			<Button variant="dangerOutline">DANGER OUTLINE</Button>
+			<Button variant="super">SUPER</Button>
+			<Button variant="superOutline">SUPER OUTLINE</Button>
+			<Button variant="ghost">ghost</Button>
+			<Button variant="sidebar">sidebar</Button>
+			<Button variant="sidebarOutline">sidebar outline</Button>
 		</div>
 	);
 }
