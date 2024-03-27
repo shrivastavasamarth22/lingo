a9bc3bd1e1836d9b065af3ba3a2ad0d76cf843e7
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-03-27 18:25:42 +0530

    created the learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,23 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { Header } from "./header";
+
 const LearnPage = () => {
 	return (
-		<div>
-			<h1>This is the learn page</h1>
+		// TODO: Try to remove the flex-row-reverse class and write sticky wrapper below feed wrapper
+		<div className="flex flex-row-reverse gap-[48px] px-6">
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+					hearts={5}
+					points={100}
+					hasActiveSubscription={false}
+				/>
+			</StickyWrapper>
+			<FeedWrapper>
+				<Header title="Spanish" />
+			</FeedWrapper>
 		</div>
 	);
 };
