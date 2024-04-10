8843f0b6c0b3153f66cbf15fa6de84faadcc3eed
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-04-10 20:43:13 +0530

    created a loading screen for the learn and courses page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,10 +2,19 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { Header } from "./header";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+	const userProgressData = getUserProgress();
+
+	const [userProgress] = await Promise.all([userProgressData]);
+
+	if (!userProgress || !userProgress.activeCourse) {
+		redirect("/courses");
+	}
+
 	return (
-		// TODO: Try to remove the flex-row-reverse class and write sticky wrapper below feed wrapper
 		<div className="flex flex-row-reverse gap-[48px] px-6">
 			<StickyWrapper>
 				<UserProgress
