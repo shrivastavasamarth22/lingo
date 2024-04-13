cd1f5a52e5b173d1cfd9fc398db8096ed7a5168e
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-04-13 10:35:38 +0530

    added data in the seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -15,6 +15,34 @@ const main = async () => {
 		await db.delete(schema.courses);
 		await db.delete(schema.userProgress);
 
+		await db.insert(schema.courses).values([
+			{
+				id: 1,
+				title: "Spanish",
+				imageSrc: "/es.svg",
+			},
+			{
+				id: 2,
+				title: "French",
+				imageSrc: "/fr.svg",
+			},
+			{
+				id: 3,
+				title: "Italian",
+				imageSrc: "/it.svg",
+			},
+			{
+				id: 4,
+				title: "Japanese",
+				imageSrc: "/jp.svg",
+			},
+			{
+				id: 5,
+				title: "Croatian",
+				imageSrc: "/hr.svg",
+			},
+		]);
+
 		console.log("Seeding finished");
 	} catch (error) {
 		console.error(error);
